0bcfe91a121ab996c58133acdebec24f86d3cc45
godori <gej.okpo@gmail.com>
2018-09-20 11:43:27 +0900

    Add MoviePoster Component and pass the data to the Component

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import './Movie.css';
 
 class Movie extends Component {
     render() {
+        console.log(this.props);
         return (
             <div>
-                Hi this is movie.
+                <h1> {this.props.title} </h1>
+                <MoviePoster poster={this.props.poster}/>
             </div>
+        )
+    }
+}
+
+class MoviePoster extends Component {
+    render() {
+        return (
+            <img src={this.props.poster} />
         )
     }
 }
